89aaa58fc02d5277fc7f791f662c6b52ab0420eb
Juanse Pérez Herrero <juanseph@gmail.com>
2020-03-23 14:42:36 +0100

    add new docker file path

--- bin/build.sh
+++ bin/build.sh
@@ -4,4 +4,4 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 source "${BASE_DIR}/bin/config.sh"
 
-docker build --file $MAIN_DOCKERFILE -t $MAIN_IMAGE:latest "${BASE_DIR}"
+docker build --file "${BASE_DIR}/$MAIN_DOCKERFILE" -t $MAIN_IMAGE:latest "${BASE_DIR}"

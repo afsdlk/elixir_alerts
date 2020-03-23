68acc559b795e303a4c308e6b6d6580d92ede74d
Juanse Pérez Herrero <juanseph@gmail.com>
2020-03-23 14:43:31 +0100

    add other volumes

--- bin/run.sh
+++ bin/run.sh
@@ -4,4 +4,11 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 source "${BASE_DIR}/bin/config.sh"
 
-docker run -t -d --rm $MAIN_CONTAINER_USER_ID $PORT_FORWARD $EXTRA_VOLUMES -v "$BASE_DIR/$HOST_ROOT":/$MAIN_ROOT --name $MAIN_CONTAINER $MAIN_IMAGE
+docker run -t -d --rm \
+  $NETWORK \
+  $MAIN_CONTAINER_USER_ID \
+  $PORT_FORWARD \
+  $EXTRA_VOLUMES \
+  -v "$BASE_DIR/$EXTRA_MOUNT":/$EXTRA_MOUNT \
+  -v "$BASE_DIR/$HOST_ROOT":/$MAIN_CONTAINER_ROOT \
+  --name $MAIN_CONTAINER $MAIN_IMAGE

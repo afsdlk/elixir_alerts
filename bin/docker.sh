159a0f90a69aa343d74ffe3c785dc88c6c53c058
Juanse Pérez Herrero <juanseph@gmail.com>
2020-03-23 14:41:07 +0100

    remove container just in case

--- bin/docker.sh
+++ bin/docker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
 (
+reset
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 ./stop.sh
+./remove.sh
 ./build.sh
 ./run.sh
 ./logs.sh
---
+++ bin/remove.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+source "${BASE_DIR}/bin/config.sh"
+
+TARGET=${1:-$MAIN_CONTAINER}
+
+docker rm $TARGET

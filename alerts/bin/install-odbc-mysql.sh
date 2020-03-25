703920999d46e3a0d21f9e604454b4565143b2c5
Juanse Pérez Herrero <juanseph@gmail.com>
2020-03-25 08:49:09 +0100

    mysql odbc driver

---
+++ alerts/bin/install-odbc-mysql.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+rm -rf /tmp/mysql-connector-odbc.*
+wget https://dev.mysql.com/get/Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.19-linux-ubuntu19.10-x86-64bit.tar.gz -O /tmp/mysql-connector-odbc.tar.gz
+tar zxvfCP /tmp/mysql-connector-odbc.tar.gz /tmp
+cp /tmp/mysql-connector-odbc-8.0.19-linux-ubuntu19.10-x86-64bit/lib/libmyodbc8* /usr/lib/x86_64-linux-gnu/odbc/
+/tmp/mysql-connector-odbc-8.0.19-linux-ubuntu19.10-x86-64bit/bin/myodbc-installer -d -a -n "MySQL" -t "DRIVER=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc8w.so;"
+rm -rf /tmp/mysql-connector-odbc.*

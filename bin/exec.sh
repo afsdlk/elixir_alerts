cf30f915489d827db89b89c3690d519fe385b121
Juanse Pérez Herrero <juanseph@gmail.com>
2020-03-25 07:22:34 +0100

    omitted paramters

--- bin/exec.sh
+++ bin/exec.sh
@@ -3,7 +3,22 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 source "${BASE_DIR}/bin/config.sh"
 
-USER=${1:-$MAIN_CONTAINER_USER_ID}
-TARGET=${2:-$MAIN_CONTAINER}
+USER=$MAIN_CONTAINER_USER_ID
+TARGET=$MAIN_CONTAINER
 
-docker exec -it $USER $TARGET bash -c "${@:3}"
+if [ "$#" -eq 1 ]; then
+  REST_STARTS_AT=1
+fi
+
+if [ "$#" -eq 2 ]; then
+  TARGET=$1
+  REST_STARTS_AT=2
+fi
+
+if [ "$#" -gt 2 ]; then
+  USER=$1
+  TARGET=$2
+  REST_STARTS_AT=3
+fi
+
+docker exec -it $USER $TARGET bash -c "${@:$REST_STARTS_AT}"

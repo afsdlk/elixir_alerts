09a61cf829d35fb33cc8bfd4164aae30d6645c83
Juanse Pérez Herrero <juanseph@gmail.com>
2020-04-03 16:31:12 +0200

    test Jobs

--- alerts/test/business/job_test.exs
+++ alerts/test/business/job_test.exs
@@ -1,20 +1,8 @@
 defmodule Business.JobsTest do
   use ExUnit.Case
-
-  defmodule SendingProcess do
-    def run(pid, name) do
-      IO.puts("CALLING ELVIS")
-      send(pid, name)
-    end
-  end
-
   require Alerts.Scheduler
+  import Crontab.CronExpression
   alias Alerts.Business.Jobs
-
-  setup do
-    # Deletes all jobs before running any test
-    Alerts.Scheduler.delete_all_jobs()
-  end
 
   defp random_name() do
     :crypto.strong_rand_bytes(32)
@@ -23,21 +11,52 @@ defmodule Business.JobsTest do
     |> String.to_atom()
   end
 
-  test "Test jobs are empty" do
-    assert Alerts.Scheduler.jobs() == []
-  end
+  defmodule(SendingProcess, do: def(run(pid, name), do: send(pid, name)))
 
-  test "Saving jobs, errors on save and, callback invocation" do
+  def setup, do: Alerts.Scheduler.delete_all_jobs()
+
+  test "Test empty", do: assert(Alerts.Scheduler.jobs() == [])
+
+  test "Test save" do
     name = random_name()
     pid = self()
-    Jobs.save(name, fn -> SendingProcess.run(pid, :hello) end, "@reboot")
+    Jobs.save(name, fn -> SendingProcess.run(pid, name) end, "@reboot")
+
+    assert_raise FunctionClauseError, ~r/^.*Quantum.Job.set_name.*$/, fn ->
+      Jobs.save("string instead of atom", fn -> :ok end, "@reboot")
+    end
+
+    assert_raise RuntimeError, ~r/^Can't parse .* as minute.$/, fn ->
+      Jobs.save(:atom, fn -> :ok end, "wrong schedule")
+    end
+
+    assert Alerts.Scheduler.find_job(name) !== nil
+    assert_receive name
+  end
+
+  test "Test delete" do
+    name = random_name()
+    Jobs.save(name, fn -> :ok end, "@reboot")
+    Jobs.delete(name)
+    assert Alerts.Scheduler.find_job(name) == nil
+  end
+
+  test "Test update" do
+    name = random_name()
+    fn1 = fn -> :ok end
+    fn2 = fn -> :ok end
+
+    Jobs.save(name, fn1, "@reboot")
+    Jobs.update(name, fn2, "* * * * *")
+
+    assert_raise RuntimeError, ~r/^Can't parse .* as minute.$/, fn ->
+      Jobs.update(:atom, fn1, "wrong schedule")
+    end
 
     # Job exists and function was invoked
-    assert Alerts.Scheduler.find_job(name) !== nil
-    assert_receive :hello
+    job = Alerts.Scheduler.find_job(name)
 
-    assert_raise FunctionClauseError, fn ->
-      Jobs.save("string not atom", fn -> SendingProcess.run(self(), name) end, "@reboot")
-    end
+    assert job.task == fn2
+    assert job.schedule == ~e[* * * * * *]
   end
 end

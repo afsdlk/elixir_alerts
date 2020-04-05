2a20248f979a924e5a30205e0bdc5d94ec5ec8bf
Juanse Pérez Herrero <juanseph@gmail.com>
2020-04-05 16:31:05 +0200

    select db by parameter

--- bin/db.sh
+++ bin/db.sh
@@ -3,4 +3,6 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${BASE_DIR}/config.sh"
 
-"${BASE_DIR}/bin/exec.sh" "$DB_USER_ID" $DB_CONTAINER "psql -U postgres $DATABASE_NAME"
+DB=${1:-$DATABASE_NAME}
+
+"${BASE_DIR}/bin/exec.sh" "$DB_USER_ID" $DB_CONTAINER "psql -U postgres $DB"
